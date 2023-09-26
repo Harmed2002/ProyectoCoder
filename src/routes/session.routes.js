3336a02b359611c6ac213499e99afe4e643cd4a2
Harold <harmed2002@hotmail.com>
2023-09-26 16:36:39 -0500

    Correcciones Login users

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -5,13 +5,14 @@ const sessionRouter = Router();
 
 sessionRouter.post("/login", async (req, res) => {
 	const { email, password } = req.body;
-console.log(email);
+
 	try {
 		if (req.session.login) {
 			res.status(200).send({ respuesta: "Login ya existente" });
 		}
 
 		const user = await userModel.findOne({ email: email });
+		// console.log("user", user)
 
 		if (user) {
 			if (user.password == password) {
@@ -23,14 +24,10 @@ console.log(email);
 
 				res.status(200).send({ respuesta: "Login validado", mensaje: user });
 			} else {
-				res
-					.status(401)
-					.send({ respuesta: "Contraseña no válida", mensaje: password });
+				res.status(401).send({ respuesta: "Contraseña no válida", mensaje: password });
 			}
 		} else {
-			res
-				.status(404)
-				.send({ respuesta: "Este Usuario no existe", mensaje: user });
+			res.status(404).send({ respuesta: "Este Usuario no existe!", mensaje: user });
 		}
 	} catch (error) {
 		res.status(400).send({ respuesta: "Error en login", mensaje: error });
